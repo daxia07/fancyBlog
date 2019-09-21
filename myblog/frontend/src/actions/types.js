a1c5b93b60af112f21b6667f659fb8ae3421e844
alienz <limingxia07@gmail.com>
2019-09-21 11:33:43 +1000

    some cleaning

--- myblog/frontend/src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const GET_POSTS = 'GET_POSTS';
-export const DELETE_POST = 'DELETE_POST';
-export const PUT_POST = 'PUT_POST';
-export const PATCH_POST = 'PATCH_POST';
-export const CREATE_POST = 'CREATE_POST';
-export const GET_ERROR = 'GET_ERRORS';
-export const SEND_MESSAGE = 'SEND_MESSAGE';
